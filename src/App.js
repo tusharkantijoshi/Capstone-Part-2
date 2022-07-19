ee27ab60b228b50698a1e0425531fe99c314b583
Tusharkanti Joshi <tusharkantijoshi@gmail.com>
2022-07-19 14:42:35 +0530

    Project Statement for the Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Home from "./components/home/Home";
 
 function App() {
-  return <div className="App">working</div>;
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
 }
 
 export default App;
